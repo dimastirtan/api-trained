0b179559c60f60a318f560a481110b018ee2d520
MR'Xoexarno <bayurizkizulfikri@gmail.com>
2023-09-19 23:13:02 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,25 +7,25 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'ka2bUiXoFhKpSZ9',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
+global.namabot = "_Zull-Assistant_"
+global.namaowner = "*BAYU RIZKI ZULFIKRI*"
 global.packname = namabot
 global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
+global.myweb = "https://wa.me/6283821362840?text=assalamualaikum"
 global.footer_text = "© " + namabot
-global.pp_bot = fs.readFileSync("./image/foto.jpg")
-global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.pp_bot = fs.readFileSync("./image/foto2.jpg")
+global.qris = fs.readFileSync("./image/qris2.jpg")
+global.owner = ['6283821362840','6281511073726']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
 global.grup_only = false //bot hanya bisa di gunakan dalam grup jika true
 global.mess = {
     done: 'Done ✅',
-    wait: '_⌛ Sedang di proses_',
+    wait: '_⌛ dagoan kedeng_',
     admin: '_Fitur Khusus Admin Group_',
     botNotAdmin: '_Jadikan Bot Sebagai Admin Terlebih Dahulu_',
     owner: '_Fitur Khusus Owner Bot_',
@@ -777,6 +777,7 @@ menu(pushname, salam, mundur, upload, download, totalGb, usedGb, freeGb, ownerNa
 ┆❐ ${prefix}antilink
 ┆❐ ${prefix}antilink2
 ┆❐ ${prefix}hidetag
+┆❐ _GA KEBAYANG NOMOR LU DI GBAN SAMA SI ARTUR KALO NYEPAM_
 ╰──────────◇
 `
 }
